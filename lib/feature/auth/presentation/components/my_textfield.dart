1a8839a473ccda5850d11003eda40b9bfceed34f
amit24af <fkaljanac1@etf.unsa.ba>
2026-04-26 19:41:18 +0200

    (auth) added basic LoginPage UI

---
+++ lib/feature/auth/presentation/components/my_textfield.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.indigo),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.indigo.shade300),
+        ),
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.indigo),
+      ),
+    );
+  }
+}
--- lib/feature/auth/presentation/pages/login_page.dart
+++ lib/feature/auth/presentation/pages/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:uat_project/feature/auth/presentation/components/my_textfield.dart';
+
+import '../components/my_button.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,12 +12,62 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final pwController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      )
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Center(
+          child: Column(
+              children: [
+              Icon(Icons.lock_open, size: 75),
+          const SizedBox(height: 25),
+          MyTextField(
+            controller: emailController,
+            hintText: "Email..",
+            obscureText: false,
+          ),
+          const SizedBox(height: 25),
+          MyTextField(
+            controller: pwController,
+            hintText: "Password..",
+            obscureText: true,
+          ),
+          const SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "Forgot password?",
+                style: TextStyle(
+                  color: Colors.indigo,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 25),
+          MyButton(onTap: () {}, text: 'Login'),
+          const SizedBox(height: 25),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Do not have an account? '),
+            Text(
+              "Register now",
+              style: TextStyle(
+                  color: Colors.indigo, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        ],
+      ),
+    ),)
+    ,
     );
   }
 }
